4ecd354530b1048c7ae8a7cc084d83f45034d11e
DucZuyVuTM <142351553+DucZuyVuTM@users.noreply.github.com>
2024-11-19 14:11:06 +0300

    Update SiAOD-PR5.1-Ex2b.cpp

--- PR5.1/4>Sorting-number-sequence_64-inf/SiAOD-PR5.1-Ex2b.cpp
+++ PR5.1/4>Sorting-number-sequence_64-inf/SiAOD-PR5.1-Ex2b.cpp
@@ -19,61 +19,61 @@ int main() {
     
     cin >> inp;
     while (inp >= 0) {
-    numbers.push_back(inp);
-    cin >> inp;
+        numbers.push_back(inp);
+        cin >> inp;
     }
     
     bool toVector = false;
     
     for (int &x : numbers) {
-       if (x > 63) {
-           toVector = true;
-       }
+        if (x > 63) {
+            toVector = true;
+        }
     }
     
     bool not_approved = false;
     int sorted_amount = 0; int old_amount = 0;
     
     for (int i = 0; i <= *std::max_element(numbers.begin(), numbers.end()); i++) {
-          for (int &x : numbers) {
-              if (i == x) {
-                  for (int &y : sorted_nums) {
-                      if (i == y) {
-                          not_approved = true;
-                          break;
-                      }
-                  }
-                  if (!not_approved) {
-                      sorted_nums.push_back(i);
-                  } else {
-                      not_approved = false;
-                  }
-              }
-          }
-          sorted_amount = sorted_nums.size();
-          if (sorted_amount == old_amount) {
-              bit_arr.push_back(0);
-          } else {
-              bit_arr.push_back(1);
-              old_amount = sorted_amount;
-          }
+        for (int &x : numbers) {
+            if (i == x) {
+                for (int &y : sorted_nums) {
+                    if (i == y) {
+                        not_approved = true;
+                        break;
+                    }
+                }
+                if (!not_approved) {
+                    sorted_nums.push_back(i);
+                } else {
+                    not_approved = false;
+                }
+            }
+        }
+        sorted_amount = sorted_nums.size();
+        if (sorted_amount == old_amount) {
+            bit_arr.push_back(0);
+        } else {
+            bit_arr.push_back(1);
+            old_amount = sorted_amount;
+        }
     }
     
     if (toVector) {
-       cout << endl << "Bit array: ";
-          for (int &x : bit_arr) {
-           cout << x;
-       }
+        cout << endl << "Bit array: ";
+        for (int &x : bit_arr) {
+            cout << x;
+        }
     } else {
-       for (int i = 1; i <= bit_size; i++) {
-           for (int &x : numbers) {
-                  if (x == (i - 1)) {
-                      bit_num = bit_num | (maska << (bit_size - i));
-                  }
-              }
-          }
-          cout << "Bit number: " << bitset<bit_size> (bit_num) << endl;
-          cout << "Number from bit number: " << bit_num << endl;
+        for (int i = 1; i <= bit_size; i++) {
+            for (int &x : numbers) {
+                if (x == (i - 1)) {
+                    bit_num = bit_num | (maska << (bit_size - i));
+                }
+            }
+        }
+        cout << "Bit number: " << bitset<bit_size> (bit_num) << endl;
+        cout << "Number from bit number: " << bit_num << endl;
     }
     return 0;
 }
